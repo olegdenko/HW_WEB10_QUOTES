bc1db19a28bbb7dc7428dc0d3cf36c802c50d4b5
Denko <officedenko@gmail.com>
2023-10-24 00:16:52 +0300

    authors_is_working

--- hw_quotes/quotes/templatetags/extract.py
+++ hw_quotes/quotes/templatetags/extract.py
@@ -1,15 +1,15 @@
-# from bson.objectid import ObjectId
-# from django import template
+from bson import ObjectId
+from django import template
 
-# from ..utils import get_mongo_db
+from ..utils import get_mongodb
 
-# register = template.Library()
-
-
-# def get_author(id_):
-#     db = get_mongo_db()
-#     author = db.authors.find_one({'_id': ObjectId(id_)})
-#     return author['fullname']
+register = template.Library()
 
 
-# register.filter('author', get_author)
+def get_author(id_):
+    db = get_mongodb()
+    author = db.authors.find_one({'_id': ObjectId(id_)})
+    return author['fullname']
+
+
+register.filter('author', get_author)
