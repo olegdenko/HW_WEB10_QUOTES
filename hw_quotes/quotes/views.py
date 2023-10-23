3924a28ea149c40e5f7462b0ef67de2ad5891ecb
Denko <officedenko@gmail.com>
2023-10-23 23:37:46 +0300

    doesent_work_pymongo

--- hw_quotes/quotes/views.py
+++ hw_quotes/quotes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .utils import get_mongo_db
+# from .utils import get_mongo_db
 
 # Create your views here.
 
 def main(request):
-    db = get_mongo_db()
-    quotes = db.quotes.find()
-    return render(request, 'quotes/index.html', context={'quotes': quotes})
+    # db = get_mongo_db()
+    # quotes = db.quotes.find()
+    return render(request, 'quotes/index.html', context={})
+    # return render(request, 'quotes/index.html', context={'quotes': quotes})
--- hw_quotes/utils/add_quotes_to_mongo.py
+++ hw_quotes/utils/add_quotes_to_mongo.py
@@ -1,17 +1,17 @@
-import json
-from bson.objectid import ObjectId
+# import json
+# from bson.objectid import ObjectId
 
-from pymongo import MongoClient
+# from pymongo import MongoClient
 
 
-client = MongoClient("mongodb://localhost")
+# client = MongoClient("mongodb://localhost")
 
-db = client.hw
+# db = client.hw
 
-with open('quotes.json', 'r', encoding='utf-8') as fd:
-    quotes = json.load(fd)
+# with open('quotes.json', 'r', encoding='utf-8') as fd:
+#     quotes = json.load(fd)
 
-for quote in quotes:
-    author = db.authors.find_one({'fullname': quote['author']})
-    if author:
-        db.quotes.insert_one({'quote': quote['quote'], 'tags': quote['tags'], 'author': ObjectId(author['_id'])})
+# for quote in quotes:
+#     author = db.authors.find_one({'fullname': quote['author']})
+#     if author:
+#         db.quotes.insert_one({'quote': quote['quote'], 'tags': quote['tags'], 'author': ObjectId(author['_id'])})
